6c7bfa109eaf90180d342a13c687a5fd2e160897
tripplen23 <tripplen63@gmail.com>
2023-01-02 23:59:56 +0200

    keep calm and commit 2

--- blog/src/index.js
+++ blog/src/index.js
@@ -39,6 +39,6 @@ app.set('views', path.join(__dirname, 'resources/views'));
 // Routes init
 route(app);
 
-            app.listen(port, () => {
-                console.log(`Example app listening on port ${port}`);
-            });
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
