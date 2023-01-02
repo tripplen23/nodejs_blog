7a7252809647d3c03a7e47d766b381623c791d94
tripplen23 <tripplen63@gmail.com>
2023-01-02 22:54:10 +0200

    message

--- blog/src/index.js
+++ blog/src/index.js
@@ -31,7 +31,6 @@ app.engine(
 );
 app.set(
     'view engine',
-
     'hbs',
 );
 
@@ -43,6 +42,6 @@ app.set('views', path.join(__dirname, 'resources/views'));
 // Routes init
 route(app);
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+                    app.listen(port, () => {
+                        console.log(`Example app listening on port ${port}`);
+                    });
