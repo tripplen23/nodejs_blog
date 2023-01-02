954f480019969d475d0e9731a2ac417323bd8d49
tripplen23 <tripplen63@gmail.com>
2023-01-02 23:34:22 +0200

    This is a new commit for what I originally planned to be amended

--- blog/src/index.js
+++ blog/src/index.js
@@ -29,10 +29,7 @@ app.engine(
         extname: '.hbs',
     }),
 );
-app.set(
-    'view engine',
-    'hbs',
-);
+app.set('view engine', 'hbs');
 
 // Cấu hình resources/views
 app.set('views', path.join(__dirname, 'resources/views'));
